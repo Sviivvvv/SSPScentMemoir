b4ebe3103be15b59b5372bc18b6dcf954c80ee7e
Sviivvvv <savindu802@gmail.com>
2025-10-03 05:56:00 +0530

    conditional Mongo provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,12 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->singleton(CartManager::class, fn() => new CartManager());
+
+        $shouldEnableMongo = env('ENABLE_MONGO', app()->environment('local'));
+
+        if (extension_loaded('mongodb') && $shouldEnableMongo) {
+            $this->app->register(\MongoDB\Laravel\MongoDBServiceProvider::class);
+        }
     }
 
     public function boot(): void
